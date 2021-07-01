df8405333ca2db8d586b964a31207c6a8208f773
saidmsaif <saidmsaiff@gmail.com>
2021-07-01 10:54:39 +0600

    Comming Soon Page Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,26 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      backgroundImage: theme => ({
-        'contact-page-hero': "url('https://cdn.shopify.com/s/files/1/0464/2281/8974/files/close-up-2021-stationary.jpg?v=1614749527')",
-       })
-    },
-    // backgroundColor: theme => ({
-    //   'primary': '#FBA028',
-    //  }),
+	purge: ["./src/**/*.html", "./src/**/*.js"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			backgroundImage: (theme) => ({
+				"contact-page-hero":
+					"url('https://cdn.shopify.com/s/files/1/0464/2281/8974/files/close-up-2021-stationary.jpg?v=1614749527')",
+				"landing-page": "url(../images/landing-page-bg.jpg);",
+			}),
+		},
 
-    //  borderColor: theme => ({
-    //   'primary': '#FFA137',
-    //  })
-  },
-  
-  variants: {
-    extend: {
-      display: ["hover", "focus", "group-hover"],
-      transitionDuration: ['hover', 'focus'],
-    },
-  },
-  plugins: [],
-}
+		textColor: {
+			primary: "#E2831B",
+		},
+	},
+
+	variants: {
+		extend: {
+			display: ["hover", "focus", "group-hover"],
+			transitionDuration: ["hover", "focus"],
+			textColor: ["active"],
+		},
+	},
+	plugins: [],
+};
